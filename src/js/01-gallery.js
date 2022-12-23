4c6502f281b7862296f4e4fb24c11e622af839df
tooreagen <tooreagen@gmail.com>
2022-12-23 13:20:13 +0200

    add markup

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,5 +2,15 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(SimpleLightbox);
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems.map((itemImg) => {
+    return `<a class="gallery__item" href="${itemImg.original}">
+                <img class="gallery__image" src="${itemImg.preview}" alt="${itemImg.description}" />
+            </a>`
+}).join("");
+
+gallery.innerHTML = markup;
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250, });
