c20513416fab9d37ef74608eb333237bf0f1d37a
tooreagen <tooreagen@gmail.com>
2022-12-24 13:36:52 +0200

    test player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,7 @@
 import VimeoPlayer from "@vimeo/player";
 
 const iframe = document.querySelector("#vimeo-player");
-const player = new VimeoPlayer.Player(iframe);
+const player = new VimeoPlayer(iframe);
 
 player.on('play', function() {
     console.log('played the video!');
