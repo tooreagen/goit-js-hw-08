b54cccbecf0a7a44063a68723b07806b2132cf81
tooreagen <109894114+tooreagen@users.noreply.github.com>
2022-12-24 11:22:29 +0000

    Deploying to gh-pages from @ tooreagen/goit-js-hw-08@5a054296bad18f895b97c9ca5d7d132a4cfbe567 🚀

--- 02-video.69821574.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("#vimeo-player"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)})),console.log(o)}();
-//# sourceMappingURL=02-video.69821574.js.map
