d53dd815279d3a395d7c667880ae382650425331
tooreagen <tooreagen@gmail.com>
2022-12-24 18:02:11 +0200

    test

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,8 +3,8 @@ import VimeoPlayer from "@vimeo/player";
 const iframe = document.querySelector("#vimeo-player");
 const player = new VimeoPlayer(iframe);
 
-player.on('timeupdate', function() {
-    player.getCurrentTime().then(function (seconds) {
-        console.log(`Видео на ${seconds}`);
-    });
+player.on('timeupdate', function(seconds) {
+    // player.getCurrentTime().then(function (seconds) {
+         console.log(`Видео на ${seconds}`);
+    // });
 });
