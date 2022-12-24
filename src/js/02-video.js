73f7c5aeab9c3e5f7e68d78882851efec716b59b
tooreagen <tooreagen@gmail.com>
2022-12-24 10:35:45 +0200

    add vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
 
+const iframe = document.querySelector("#vimeo-player");
+    // const player = new Vimeo.Player(iframe);
+
+    // player.on('play', function() {
+    //     console.log('played the video!');
+    // });
+
+    // player.getVideoTitle().then(function(title) {
+    //     console.log('title:', title);
+    // });
+
+
+    console.log("iframe");
+    console.log(iframe);
