d1c02fc1f382cc70cb115544e8bf4cc71dd31fa4
tooreagen <tooreagen@gmail.com>
2022-12-25 14:44:09 +0200

    values to storage

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,13 +4,14 @@ const saveDataToStorage = (event) => {
     // const { elements: { email, message } } = event.currentTarget;
     localStorage.setItem("feedback-form-state", event.currentTarget.elements);
 
-    console.log("Значение elements = ", JSON.stringify(localStorage.getItem("feedback-form-state")));
+    console.log("Значение elements с stringify = ", JSON.stringify(localStorage.getItem("feedback-form-state")));
+    console.log("Значение elements = ", localStorage.getItem("feedback-form-state"));
 }
 
 form.addEventListener("input", saveDataToStorage);
 
 
-console.log(3);
+console.log(4);
 
 
 // В HTML есть разметка формы. Напиши скрипт который будет сохранять значения полей в локальное хранилище когда пользователь что-то печатает.
