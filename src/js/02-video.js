d0dc48455e563d7b421d83caea7f559954b1d962
tooreagen <tooreagen@gmail.com>
2022-12-25 10:57:37 +0200

    throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,6 +9,4 @@ player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
 
 player.on('timeupdate', throttle(function (data) {
     localStorage.setItem("videoplayer-current-time", data.seconds);
-    console.log(`Видео на ${data.seconds}`);
-    console.log("Прочитано из хранилища = ", localStorage.getItem("videoplayer-current-time"));
 },1000));
