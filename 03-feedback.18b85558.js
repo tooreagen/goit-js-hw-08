3bf45839f9f395d00f2b3e9fb1bf3b6dc14e7625
tooreagen <109894114+tooreagen@users.noreply.github.com>
2022-12-25 10:55:21 +0000

    Deploying to gh-pages from @ tooreagen/goit-js-hw-08@6465683e6cdf294044595559570ee9304201217c 🚀

---
+++ 03-feedback.18b85558.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form").addEventListener("input",(function(e){var t=e.currentTarget.elements;t.email,t.message,localStorage.setItem("elements",elements),console.log("Значение elements = ",localStorage.getItem("elements"))})),console.log(1);
+//# sourceMappingURL=03-feedback.18b85558.js.map
