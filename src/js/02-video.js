050e76f09fb1548c15082fd60395493bcd1d15d8
tooreagen <tooreagen@gmail.com>
2022-12-25 10:10:13 +0200

    localStorage

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,8 +3,19 @@ import VimeoPlayer from "@vimeo/player";
 const iframe = document.querySelector("#vimeo-player");
 const player = new VimeoPlayer(iframe);
 
-player.on('timeupdate', function(data) {
-    // player.getCurrentTime().then(function (seconds) {
-         console.log(`Видео на ${data.seconds}`);
-    // });
+player.on('timeupdate', function (data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+    console.log(`Видео на ${data.seconds}`);
+    console.log("Прочитано из хранилища = ", localStorage.getItem("videoplayer-current-time"));
 });
+
+
+
+
+
+
+// player.on('timeupdate', function(data) {
+//     // player.getCurrentTime().then(function (seconds) {
+//          console.log(`Видео на ${data.seconds}`);
+//     // });
+// });
