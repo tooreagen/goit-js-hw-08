8fc55a7980f318202726b025603b25341009753d
tooreagen <109894114+tooreagen@users.noreply.github.com>
2022-12-25 13:29:59 +0000

    Deploying to gh-pages from @ tooreagen/goit-js-hw-08@07536f67f6d5f855fc4fdcfd30efd4e849ecd6da 🚀

--- 03-feedback.85de7df0.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form").addEventListener("input",(e=>{localStorage.setItem("feedback-form-state",e.currentTarget.elements),console.log(e.currentTarget.elements)})),console.log(5);
-//# sourceMappingURL=03-feedback.85de7df0.js.map
