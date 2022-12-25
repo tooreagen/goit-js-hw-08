cc5ab127ff9c2249ba6a3cda3f900b327f4c4f37
tooreagen <tooreagen@gmail.com>
2022-12-25 10:30:41 +0200

    localStorage

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,6 +3,9 @@ import VimeoPlayer from "@vimeo/player";
 const iframe = document.querySelector("#vimeo-player");
 const player = new VimeoPlayer(iframe);
 
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+
 player.on('timeupdate', function (data) {
     localStorage.setItem("videoplayer-current-time", data.seconds);
     console.log(`Видео на ${data.seconds}`);
