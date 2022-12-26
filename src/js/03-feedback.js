8b5a9eab5654ce69587d1c418f7db71683e75f8d
tooreagen <tooreagen@gmail.com>
2022-12-26 18:16:25 +0200

    updateDataUser

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -16,7 +16,7 @@ const saveDataToStorage = (event) => {
 }
 
 function updateDataUser() {
-    const dataFromStorage = JSON.parse(localStorage.getItem(KEY_STORAGE) || (form.email.value="пустые поля"));
+    const dataFromStorage = JSON.parse(localStorage.getItem(KEY_STORAGE)) || (form.email.value="ПУТИН ХУЙЛО");
 
     form.email.value = dataFromStorage.email;
     form.message.value = dataFromStorage.message;
