5e2713791f2ff3331ca5ea090a04695bdf711c43
tooreagen <109894114+tooreagen@users.noreply.github.com>
2022-12-26 17:06:00 +0000

    Deploying to gh-pages from @ tooreagen/goit-js-hw-08@ef5e7997a341fb92041c943a13ef9c645fbde46f 🚀

--- 03-feedback.0910decd.js
+++
@@ -1,2 +0,0 @@
-!function(){console.log(16);var e,a=document.querySelector(".feedback-form"),t="feedback-form-state";e=JSON.parse(localStorage.getItem(t))||"",a.email.value=e.email,a.message.value=e.message,console.log(e),a.addEventListener("input",(function(e){var a=e.currentTarget.elements,l=a.email,o=a.message,r={email:l.value,message:o.value};localStorage.setItem(t,JSON.stringify(r))})),a.addEventListener("submit",(function(e){e.preventDefault(),localStorage.clear(),a.reset()}))}();
-//# sourceMappingURL=03-feedback.0910decd.js.map
--- 03-feedback.2b75f0ab.js
+++
@@ -1,2 +0,0 @@
-console.log(16);const e=document.querySelector(".feedback-form");!function(){const t=JSON.parse(localStorage.getItem("feedback-form-state"))||"";e.email.value=t.email,e.message.value=t.message,console.log(t)}(),e.addEventListener("input",(e=>{const{elements:{email:t,message:a}}=e.currentTarget,s={email:t.value,message:a.value};localStorage.setItem("feedback-form-state",JSON.stringify(s))})),e.addEventListener("submit",(function(t){t.preventDefault(),localStorage.clear(),e.reset()}));
-//# sourceMappingURL=03-feedback.2b75f0ab.js.map
