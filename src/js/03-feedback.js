71ce59ebff5cc8d4559c2b41d61a86b5c4932ea5
tooreagen <tooreagen@gmail.com>
2022-12-27 20:38:38 +0200

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,7 +2,7 @@ import throttle from "lodash.throttle";
 
 const form = document.querySelector(".feedback-form");
 const KEY_STORAGE = "feedback-form-state";
-const dataFromUser = {};
+let dataFromUser = {};
 
 updateDataUser();
 
@@ -10,9 +10,6 @@ form.addEventListener("input", throttle(saveDataToStorage, 500));
 form.addEventListener("submit", submitForm);
 
 function saveDataToStorage(event) {
-    // const formData = new FormData(form);
-    // formData.forEach((value, name) => console.log(value, name));
-
     dataFromUser[event.target.name] = event.target.value;
     localStorage.setItem(KEY_STORAGE, JSON.stringify(dataFromUser));
 }
@@ -33,5 +30,6 @@ function submitForm(event) {
     event.preventDefault();
     console.log(dataFromUser);
     localStorage.removeItem(KEY_STORAGE);
+    dataFromUser = {};
     event.currentTarget.reset();
 }
